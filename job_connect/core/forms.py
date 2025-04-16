1d84cb96f444aac8ed4020dc0678472495099879
armydillo2025 <58220923+armydillo2025@users.noreply.github.com>
2025-04-16 14:54:10 -0600

    Core tests v5
    FAILED (failures=6, errors=5)

--- job_connect/core/forms.py
+++ job_connect/core/forms.py
@@ -1,5 +1,6 @@
-#from django import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group  # Import Group
 from core.models import User
 
 class ApplicantSignUpForm(UserCreationForm):
@@ -11,6 +12,8 @@ class ApplicantSignUpForm(UserCreationForm):
         user.user_type = 'applicant'
         if commit:
             user.save()
+            applicant_group = Group.objects.get(name='Applicant')  # Get Applicant group
+            user.groups.add(applicant_group)  # Add user to group
         return user
 
 class RecruiterSignUpForm(UserCreationForm):
@@ -22,5 +25,6 @@ class RecruiterSignUpForm(UserCreationForm):
         user.user_type = 'recruiter'
         if commit:
             user.save()
+            recruiter_group = Group.objects.get(name='Recruiter')  # Get Recruiter group
+            user.groups.add(recruiter_group)  # Add user to group
         return user
-    
