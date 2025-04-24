d4065729a816d3b84e630ef367f8455b2350b416
Devin Haggitt <uofa0603@yahoo.com>
2025-04-23 23:37:34 -0600

    Making the compose actually reference the entrypoint file

--- job_connect/docker-entrypoint.sh
+++ job_connect/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 # Collect static files
 echo "Migrate from SQLite to PostgreSQL"
-python manage.py makemigrations --merge --noinput
-python manage.py migrate
+python3 manage.py makemigrations --merge --noinput
+python3 manage.py migrate
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
